6a33a811c488ad58bdd2e7755ecb7786c8ba6087
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-05-30 13:31:01 +0530

    Add

---
+++ source/n2n3n4.c
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	int n2=n*n;
+
+	int n3=n2*n;
+
+	int n4=n3*n;
+
+	cout<<n2<<endl;
+	cout<<n3<<endl;
+	cout<<n4<<endl;
+}
