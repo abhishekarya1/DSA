d74f938f5473623750a39dd6f867d9fddbb13ee5
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-04 21:19:38 +0530

    Add

--- source/midcode4/bubblesort.cc
+++ source/midcode4/bubblesort.cc
@@ -20,9 +20,9 @@ int bubble_sort(int arr[], int n)
 	for(int i=0;i<n-1;i++)
 	{
 		for(int j=0;j<n-1-i;++j)
-		{https://wakatime.com/
-			if(arr[j]<arr[j+1]) 
-				{swap(arhttps://wakatime.com/r[j],arr[j+1]);
+		{
+			if(arr[j]>arr[j+1]) 
+				{swap(arr[j],arr[j+1]);
 			flag=false;}
 		}
 		if(flag==true) break;
