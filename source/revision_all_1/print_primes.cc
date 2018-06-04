00c926d6df202f6720ed5a0cc61d288512dfd0d7
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-04 18:07:28 +0530

    Add

---
+++ source/revision_all_1/print_primes.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,m;
+	cin>>m>>n;
+	
+	for(int i=m;i<n;i++)
+	{
+		bool isPrime=true;
+		for(int j=2;j<i;++j)
+		{
+			if(i%j==0)
+			{ isPrime=false;
+				break;
+			}
+		}
+		if(isPrime==true) cout<<i<<" "; 
+	}
+}
