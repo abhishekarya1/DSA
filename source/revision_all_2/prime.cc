5c3beb5af9e6bb294c6ca68eb5d856d74dda68ff
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-09 17:41:20 +0530

    Add

---
+++ source/revision_all_2/prime.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	bool flag=true;
+	for(int i=2;i<n;i++)
+	{
+		if(n%i==0) { flag=false; break; }
+	}
+
+	flag==false?cout<<"Not Prime":cout<<"Prime";
+}
