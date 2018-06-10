481aba0ad8dcc204f3364e29dc956728c79c6f53
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-11 00:49:51 +0530

    Add

--- source/recursion_problems/triangle.cc
+++ source/recursion_problems/triangle.cc
@@ -4,13 +4,16 @@ using namespace std;
 
 void tri_pat(int n)
 {
-	cout<<"*";
-	tri_pat(n+1);
+	if (n <= 0) return;
+	tri_pat(n - 1);
+	for(int i=0;i<n;i++) cout << "*" << "\t";
+
+	cout<<endl;
 }
 
 int main()
 {
 	int n;
-	cin>>n;
+	cin >> n;
 	tri_pat(n);
 }
