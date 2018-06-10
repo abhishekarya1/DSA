27c23dfefd8e9c6e30eb4b2757fd3206a26ffb86
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-11 01:53:01 +0530

    Add

---
+++ source/recursion_problems/search.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+void input_array(int *arr, int n)
+{
+	for (int i = 0; i < n; i++) cin >> arr[i];
+}
+
+bool isThere(int *arr, int n, int m)
+{
+	if (arr[n - 1] == m) return true;
+	else isThere(arr, n - 1, m); return false;
+	
+}
+
+int main()
+{
+	int arr[10], n, m;
+
+	cin >> n;
+	input_array(arr, n);
+	cin >> m;
+	if (isThere(arr, n, m) == true) cout << "true";
+	else cout << "false";
+
+}
