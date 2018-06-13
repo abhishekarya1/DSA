7e3e5e038f85259a84a5afe71dd25b5ffe42ecb1
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-13 17:53:42 +0530

    Add

--- source/recursion_problems/towerofhanoi.cc
+++ source/recursion_problems/towerofhanoi.cc
@@ -2,10 +2,21 @@
 
 using namespace std;
 
+int cnt = 0;
+
+void printSteps(int n, char src, char dest, char helper)
+{
+	if (n == 0) return;
+	cnt++;
+	printSteps(n - 1, src, helper, dest);
+	cout << "Step - " << cnt << " " << n << " : " << src << " --> " << dest << endl;
+	printSteps(n - 1, helper, dest, src);
+}
+
 int main()
 {
 	int n;
-	cin>>n;
+	cin >> n;
 
-	printSteps(n, 'A', 'B', 'C')
+	printSteps(n, 'A', 'B', 'C');
 }
