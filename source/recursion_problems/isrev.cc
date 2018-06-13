9677f017b5195aa926d0cd850ee56523565a29d8
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-13 16:03:24 +0530

    Add

--- source/recursion_problems/isrev.cc
+++ source/recursion_problems/isrev.cc
@@ -2,15 +2,29 @@
 
 using namespace std;
 
-bool isRev(char str1[], char str2[])
+bool isRev(char str1[], char str2[], int cur, int n)
 {
+	if (cur > n) return true;
 
+	if (str1[cur] != str2[n - 1]) return false;
+
+
+	isRev(str1, str2, cur + 1, n - 1);
 }
 
 int main()
 {
 	char str1[10], str2[10];
+	int cur = 0, i = 0;
 
-	cout << isRev(str1, str2);
+	cin>>str1;
+	cin>>str2;
+
+	while (str2[i] != '\0')
+	{
+		i++;
+	}
+
+	isRev(str1, str2, cur, i) == true ? cout<<"true" : cout<<"false";
 
 }
