e6e3a39a0a07d81c02434620c2a368f1fe7c6d48
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-13 18:09:04 +0530

    Add

--- source/recursion_problems/tohanoi2.cc
+++ source/recursion_problems/tohanoi2.cc
@@ -4,7 +4,10 @@ using namespace std;
 
 void printStepstosolve(int n, char src, char dest, char helper)
 {
-	
+	if (n == 0) return;
+	printStepstosolve(n - 1, src, helper, dest);
+	cout <<"Moving ring "<< n << " from " << src <<" to " << dest << endl;
+	printStepstosolve(n - 1, helper, dest, src);
 }
 
 int main()
