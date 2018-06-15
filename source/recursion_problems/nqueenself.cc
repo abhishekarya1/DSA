2544798461ce768e0a6fb3831c7bfd6383d08c54
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-15 21:02:49 +0530

    Add

--- source/recursion_problems/nqueenself.cc
+++ source/recursion_problems/nqueenself.cc
@@ -2,62 +2,32 @@
 
 using namespace std;
 
-void clearBoard(char board[][100], int r, int c)
-{
-	for (int i = 0; i < r; i++)
-	{
-		for (int j = 0; j < c; j++) board[i][j] = 'X';
-	}
-}
-
-void output_solved(char board[][100], int n)
+void clearBoard(char board[][100], int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++) cout << board[i][j] << " ";
-		cout << endl;
-	}
-
-}
-bool canPlace(char board[][100], int r, int c, int dim)
-{
-	for (int i = 0; i < dim; i++)
-	{
-		if (board[i][c] == 'Q') return false;
-	}
-
-	int rowDir[] = { -1, +1};
-	int colDir[] = { -1, +1};
-	for (int dir = 0; dir < 2; dir++)
-	{
-		for (int dist = 0; dist < dim; dist++)
-			
-		{	int nextRow = r + dist * rowDir[dir];
-			int nextCol = c + dist * colDir[dir];
-
-		if (nextRow >= dim || nextRow < 0 || nextCol > 0 || nextCol >= dim) break;
-
-		if (board[nextRow][nextCol] == 'Q') return false;
+		for (int j = 0; j < n; j++) board[i][j] = 'X';
 	}
 }
 
-	return true;
 
-}
 
 bool solveQueen(char board[][100], int r, int dim)
 {
-	for (int c = 0; c < dim; c++)
+	if (r == dim) return false;
+
+	for (int c; c < dim; c++)
 	{
 		bool check = canPlace(board, r, c, dim);
 		if (check == true)
-		{	board[r][c] = 'Q';
+		{
+			board[r][c] = 'Q';
+
 			bool status = solveQueen(board, r + 1, dim);
 			if (status == true) return true;
 			board[r][c] = 'X';
 		}
 	}
-
 	return false;
 }
 
@@ -67,11 +37,8 @@ int main()
 	cin >> n;
 
 	char board[100][100];
-	clearBoard(board, n, n);
+	clearBoard(board, n);
 
-	solveQueen(board, 0, n);
+	solveQueen(board, 0, n) == true ? outputBoard(board, n) : cout << "Couldn't Solve!!!";
 
-	output_solved(board, n);
-
-	return 0;
 }
