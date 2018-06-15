ae67724fe3cdf0bd6d55657848bbc8e70f02452f
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-15 22:31:13 +0530

    Add

---
+++ source/recursion_problems/subsequences.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+void printSub(char *instr, char *outstr, int length)
+{
+	
+}
+
+int main()
+{
+	char instr[100], outstr;
+
+	cin >> instr;
+
+	int length = strlen(instr);
+
+	printSub(instr, outstr, length);
+}
