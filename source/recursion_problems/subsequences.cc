66e657f883084d8c0e4fe07232e1869cbebe566e
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-15 22:41:55 +0530

    Add

--- source/recursion_problems/subsequences.cc
+++ source/recursion_problems/subsequences.cc
@@ -4,21 +4,23 @@ using namespace std;
 
 void printSub(char *instr, char *outstr, int i, int j)
 {
-	if(str[i] == '\0')
+	if (instr[i] == '\0')
 	{
-		outstr = '\0';
-		cout<<outstr<<endl;
+		outstr[j] = '\0';
+		cout << outstr << ", ";
 		return;
 	}
 
-	
+	outstr[j] = instr [i];
+	printSub(instr, outstr, i + 1, j + 1);
+	printSub(instr, outstr, i + 1, j);
 }
 
 int main()
 {
-	char instr[100], outstr;
+	char instr[100], outstr[100];
 
 	cin >> instr;
 
-	printSub(instr, outstr);
+	printSub(instr, outstr, 0, 0);
 }
