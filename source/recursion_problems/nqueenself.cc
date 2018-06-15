06bcd94dc459cfd1233936a2c0657906fd60325e
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-15 18:44:31 +0530

    Add

--- source/recursion_problems/nqueenself.cc
+++ source/recursion_problems/nqueenself.cc
@@ -10,17 +10,40 @@ void clearBoard(char board[][100], int r, int c)
 	}
 }
 
+bool canPlace(char board[][100], int r, int c, int dim)
+{
+	for(int i=0;i<dim;i++)
+	{
+		if(board[i][c] == 'Q') return false;
+	}
+
+	int rowDir[] = {-1, +1};
+	int colDir[] = {-1, +1};
+	for(int dir=0;dir<2;dir++)
+	{
+		for(int dist=0;dist<dim;dist++)
+			int nextRow = r + dist*rowDir;
+			int nextCol = c + dist*colDir;
+
+		
+	}
+
+}
+
 bool solveQueen(char board[][100], int r, int dim)
 {
 	for (int c = 0; c < dim; c++)
 	{
 		bool check = canPlace(board, r, c, dim);
 		if(check == true) 
-		board[r][c] = 'Q';
+		{board[r][c] = 'Q';
 		 bool status = solveQueen(board, r+1, dim);
 		 if(status == true) return true;
 		 board[r][c] = 'X';
 	}
+}
+
+return false;
 }
 
 int main()
