326a45e4abc7112e8d33dfb56ca2f330f09deee8
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-17 18:49:03 +0530

    Add

--- source/recursion_practice/merge_sort2.cc
+++ source/recursion_practice/merge_sort2.cc
@@ -4,7 +4,13 @@ using namespace std;
 
 void input_array(int *arr, int n)
 {
-	for (int i = 0; i < n; i++) cout << arr[i];
+	for (int i = 0; i < n; i++) cin >> arr[i];
+}
+
+
+void output_array(int *arr, int n)
+{
+	for (int i = 0; i < n; i++) cout << arr[i] << " ";
 }
 
 void merge(int *arr , int s, int e)
@@ -14,29 +20,37 @@ void merge(int *arr , int s, int e)
 	int j = mid + 1;
 	int k = s;
 
-	while (s <= mid && e >= mid + 1)
+	int tmp[100];
+
+	while (i <= mid && j <= e)
 	{
-		
+		if (arr[i] > arr[j]) tmp[k++] = arr[j++];
+		else tmp[k++] = arr[i++];
+	}
+		while (i <= mid) tmp[k++] = arr[i++];
+		while (i <= mid) tmp[k++] = arr[j++];
+		for (int i = s; i <= e; i++) {
+			arr[i] = tmp[i];
 	}
 }
+	void mergesort(int *arr, int s, int e)
+	{
+		if (s >= e) return;
 
-void mergesort(int *arr, int s, int e)
-{
-	if (s >= e) return;
+		int mid = (s + e) / 2;
 
-	int mid = (s + e) / 2;
+		mergesort(arr, s, mid);
+		mergesort(arr, mid + 1, e);
 
-	mergesort(arr, s, mid);
-	mergesort(arr, mid + 1, e);
+		merge(arr, s, e);
+	}
 
-	merge(arr, s, e);
-}
+	int main()
+	{
+		int arr[100], n;
+		cin >> n;
 
-int main()
-{
-	int arr[100], n;
-	cin >> n;
-
-	input_array(arr, n);
-	mergesort(arr, 0, n - 1);
-}
+		input_array(arr, n);
+		mergesort(arr, 0, n - 1);
+		output_array(arr, n);
+	}
