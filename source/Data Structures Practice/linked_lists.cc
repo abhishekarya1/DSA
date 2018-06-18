50f8f2c9199f2499e7b7382bcf6e641d0fc91afc
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-18 18:10:37 +0530

    Add

--- source/Data Structures Practice/linked_lists.cc
+++ source/Data Structures Practice/linked_lists.cc
@@ -2,29 +2,15 @@
 
 using namespace std;
 
-class node
-{
+class node{
 public:
 	int data;
 	node *next;
 
-	node(int d)
-	{
-		data=d;
-		next=NULL;
-	}
-
-};
-
-void insertAtHead(node *&head, int data)
-{
-	node *head = new node(data);
-	n->next = head;
-	head = n;
+	
 }
 
 int main()
 {
-	node *head = NULL;
-	insertAtHead()	
+	
 }
