a2d6a9f94224b4d901a7edd7ace3a781489010ed
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-21 16:18:26 +0530

    Add

---
+++ source/Data Structures Practice/ll_re.cc
@@ -0,0 +1,55 @@
+#include<iostream>
+
+using namespace std;
+
+class node {
+public:
+	int data;
+	node*next;
+
+	node(int d) {
+		data = d;
+		next = NULL;
+	}
+};
+
+node* createLL()
+{
+	int x;
+	node* head = NULL;
+	node* tail = NULL;
+
+	while(true)
+	{
+		cin >> x;
+		if(x == -1) break;
+
+	node* nn = new node(x);
+	if(head == NULL){
+		head = nn;
+		tail = nn;
+	}
+	else{
+		tail -> next = nn;
+		tail = nn;
+	}
+}
+return head;
+}
+
+void print(const node*head){
+	const node* cur = head;
+	while(cur)
+	{
+		cout<<cur->data<<"-->";
+		cur = cur->next;
+	}
+	cout<<endl;
+}
+
+int main()	
+{
+	node*head = createLL();
+	print(head);
+
+}
