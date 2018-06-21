cd09c22d63c859e2b6817be6d820d3119cd7f2dd
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-21 19:52:51 +0530

    Add

--- source/Data Structures Practice/ll3.cc
+++ source/Data Structures Practice/ll3.cc
@@ -2,9 +2,52 @@
 
 using namespace std;
 
+class node{
+public:
+	int data;
+	node* next;
+	node(int d)
+	{
+		data = d;
+		next = NULL;
+	}
+};
 
+node* createLL(){
+
+int x;
+node* head =NULL;
+node* tail = NULL;
+	while(true)
+	{
+		cin >> x;
+		if(x == -1) break;
+		node* nn = new node(x);
+		if(head == NULL)
+		{
+			head= nn;
+			tail =nn;
+		}
+		else{
+		tail->next = nn;
+		tail = nn;
+	}
+}
+
+return head;
+}
+
+void print(node*head){
+	node*cur = head;
+	while(cur)
+	{
+		cout<<cur->data<<"-->";
+		cur = cur->next;
+	}
+}
 
 int main()
 {
-	
+	node*head = createLL();
+	print(head);
 }
