1d721517b7daa343dddfbb4498d18e5e0d306c8e
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-21 17:30:27 +0530

    Add

--- source/Data Structures Practice/ll_re.cc
+++ source/Data Structures Practice/ll_re.cc
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-class node{
+class node {
 public:
 	int data;
 	node* next;
@@ -17,15 +17,15 @@ public:
 void insertathead(node* &head, int d)
 {
 	node* nn = new node(d);
-	nn->next= head;
+	nn->next = head;
 	head = nn;
 }
 
 void insertatend(node* &head, int d)
 {
-	node* nn = new node(d);	
+	node* nn = new node(d);
 	node* cur = head;
-	while(cur->next!=NULL)
+	while (cur->next != NULL)
 	{
 		cur = cur->next;
 	}
@@ -35,66 +35,80 @@ void insertatend(node* &head, int d)
 
 int length(node*head)
 {
-	int len=0;
-	node* cur =head;
-	while(cur)
+	int len = 0;
+	node* cur = head;
+	while (cur)
 	{
 		cur = cur ->next;
-		len+=1;
+		len += 1;
 	}
 	return len;
 }
 
-void insertatmiddle(node*&head, int pos, int d){
-	
-	if(head == NULL || pos==0) insertathead(head, d);
-	else if(pos>length(head)) insertatend(head, d);
-	else{
-	node*cur = head;
-	node*nn = new node(d);
-	while(pos-1)
-	{
-		cur=cur->next;
-		pos--;
+void insertatmiddle(node*&head, int pos, int d) {
+
+	if (head == NULL || pos == 0) insertathead(head, d);
+	else if (pos > length(head)) insertatend(head, d);
+	else {
+		node*cur = head;
+		node*nn = new node(d);
+		while (pos - 1)
+		{
+			cur = cur->next;
+			pos--;
+		}
+		nn->next = cur->next;
+		cur->next = nn;
 	}
-	nn->next= cur->next;
-	cur->next = nn;
-}
 }
 
 bool searchRecursive(node* &head, int x)
 {
-	if(head == NULL) return false;
-	if(head->data == x) return true;
-	else{return searchRecursive(head->next, x); }
+	if (head == NULL) return false;
+	if (head->data == x) return true;
+	else {return searchRecursive(head->next, x); }
 
 }
 
 bool searchIterative(node*&head, int x)
 {
 	node* cur = head;
-	while(cur)
+	while (cur)
 	{
-		if(cur->data == x) return true;
-		cur=cur->next;
+		if (cur->data == x) return true;
+		cur = cur->next;
 	}
 
 	return false;
 }
 
 void print(node*&head)
-{	
+{
 	node* cur = head;
-	while(cur){
-		cout<<cur->data<<"-->";
+	while (cur) {
+		cout << cur->data << "-->";
 		cur = cur->next;
 	}
 }
 
 void deleteathead(node*&head)
 {
+	if (head == NULL) return;
 	node* cur = head;
-	head = cur->next; 
+	head = cur->next;
+	delete cur;
+}
+
+void deleteatend(node*head)
+{
+	node*prev = NULL;
+	node* cur = head;
+	while (cur->next)
+	{
+		prev = cur;
+		cur = cur ->next;
+	}
+	prev->next = NULL;
 	delete cur;
 }
 
@@ -109,6 +123,9 @@ int main()
 	// searchRecursive(head, 7) == true? cout<<"Found":cout<<"Not Found";
 	// searchIterative(head, 3) == true? cout<<"Found":cout<<"Not Found";
 	deleteathead(head);
+	print(head);
+	cout << endl;
+	deleteatend(head);
 	print(head);
 
 }
