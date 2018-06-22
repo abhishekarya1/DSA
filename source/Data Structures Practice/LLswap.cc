483f0645c76cfd64181b4321c60f8498af344985
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-22 15:04:29 +0530

    Add

--- source/Data Structures Practice/LLswap.cc
+++ source/Data Structures Practice/LLswap.cc
@@ -12,9 +12,53 @@ public:
 	}
 };
 
-void 
+void createLL(node*&head, int n)
+{
+	int x;
+	node*tail = head;
+	while (n) {
+		cin >> x;
+		node* nn = new node(x);
+		if (head == NULL) { head = nn; tail = nn;}
+		else {
+			tail->next = nn;
+			tail = nn;
+		}
+		n--;
+	}
+}
+
+void LLswap(node*&head, int n1, int n2)
+{
+	node *cur = head;
+	node* s1 = NULL;
+	node*s2 = NULL;
+	while (cur)
+	{
+		if (cur->data == n1) s1 = cur;
+		if (cur->data == n2) s2 = cur;
+		cur = cur->next;
+	}
+	swap(s1->data,s2->data);
+}
+
+void print(node*&head)
+{
+	node* cur = head;
+	while (cur)
+	{
+		cout << cur->data << " ";
+		cur = cur->next;
+	}
+}
 
 int main()
 {
-
+	int n, n1, n2;
+	node*head = NULL;
+	cin >> n;
+	createLL(head, n);
+	cin >> n1 >> n2;
+	LLswap(head, n1, n2);
+	print(head);
 }
