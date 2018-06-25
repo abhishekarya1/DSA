c3a9989ac218203b1978d31c4f6e073a0ee1dcba
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-06-25 15:46:11 +0530

    Add

---
+++ source/HB/countpalin.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int cnt;
+
+int getCount(char str[], int len)
+{
+	bool flag = true;
+	for (int first = 0; first < len; first++)
+	{
+		for (int last = len - 1; last > first; last--)
+		{
+			int i = first;
+			int j = last;
+			while (i <= j)
+			{
+				if (str[i] != str[j]) 
+					{
+						flag = false; 
+						break;
+					} 
+			i++; 
+			j--;
+			}
+			
+			if (flag == true)
+			{
+				for (int k = first; k < last; k++) cout << str[k] << " ";
+			}
+		}
+	}
+}
+
+int main()
+{
+	char str[100];
+	int i = 0;
+	cin >> str;
+	int len = strlen(str);
+	getCount(str, len);
+}
