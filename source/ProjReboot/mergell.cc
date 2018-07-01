b4b44bef4d4e9810d02b80d775d1c9b020084817
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-07-01 16:48:09 +0530

    Add

--- source/ProjReboot/mergell.cc
+++ source/ProjReboot/mergell.cc
@@ -14,12 +14,12 @@ public:
 	}
 };
 
-node* createLL()
+node* createLL(int n)
 {
 	int x;
 	node* head = NULL;
 	node*tail = NULL;
-	while (true)
+	while (n)
 	{
 		cin >> x;
 		if (x == -1) break;
@@ -34,6 +34,7 @@ node* createLL()
 			tail->next = nn;
 			tail = nn;
 		}
+		n--;
 	}
 	return head;
 }
@@ -50,25 +51,40 @@ void print(const node* head)
 
 node* mergell(node* head1, node* head2)
 {
-	if(head1 == NULL) return head2;
-	if(head2 == NULL) return head1;
-	if(head1->data < head2->data)
+	if (head1 == NULL) return head2;
+	if (head2 == NULL) return head1;
+	if (head1->data < head2->data)
 	{
 		head1->next = mergell(head1->next, head2);
 		return head1;
 	}
 	else {
-        head2->next = mergell(head1, head2->next);
-        return head2;
-    }
+		head2->next = mergell(head1, head2->next);
+		return head2;
+	}
 
 }
 
 int main()
 {
-	node* head1  = createLL();
-	print(head1);
-	node* head2 = createLL();
-	print(head2);
-	mergell(head1, head2);
+	int n;
+	cin >> n;
+	//cout << endl;
+	while (n)
+	{
+		int n1, n2;
+		cin >> n1;
+		//cout << endl;
+		node* head1  = createLL(n1);
+		//print(head1);
+		//cout << endl;
+		cin >> n2;
+		//cout << endl;
+		node* head2 = createLL(n2);
+		//print(head2);
+		//cout << endl;
+		node* mhead = mergell(head1, head2);
+		print(mhead);
+		n--;
+	}
 }
