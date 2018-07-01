b8f0a078b97611cb287e379d83d57e2c245bf7e3
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-07-01 22:41:44 +0530

    Add

--- source/ProjReboot/poin.cc
+++ source/ProjReboot/poin.cc
@@ -6,14 +6,10 @@ int main()
 {
 	int arr[10];
 	int *ptr = &arr[0];
-	cout << ptr; // first element
+	cout << arr;
+	cout << ptr;
+	cout << ptr + 1; // first element + 1
 	cout << endl;
-	cout << arr; // address of array
-
-	cout<<endl;
-
-	cout << ptr+1; // first element + 1
-	cout << endl;
-	cout << arr+1; // address of array + 1
+	cout << arr + 1; // address of array + 1
 
 }
