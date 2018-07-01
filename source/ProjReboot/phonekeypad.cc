8eb9d8c28ae349e45f25deffb4a774bc2c6d802d
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-07-02 00:24:39 +0530

    Add

--- source/ProjReboot/phonekeypad.cc
+++ source/ProjReboot/phonekeypad.cc
@@ -1,7 +1,11 @@
 #include<iostream>
 #include<cstring>
+#include<vector>
+#include<cstring>
 
 using namespace std;
+
+vector<string> v;
 
 char keypad [][10] = {"", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WX", "YZ"};
 
@@ -10,19 +14,19 @@ void printKeypad(char *in, char* out, int i, int j)
 	if (in[i] == '\0')
 	{
 		out[j] = '\0';
-		cout << out << " ";
+		v.push_back(out);
 		return;
 	}
 
 	int digit  = in[i] - '0';
-	if(digit == 0)
+	if (digit == 0)
 	{
-		printKeypad(in, out, i+1, j);
+		printKeypad(in, out, i + 1, j);
 	}
 	for (int k = 0; keypad[digit][k] != '\0'; k++)
 	{
 		out[j] = keypad[digit][k];
-		printKeypad(in, out, i+1, j+1);
+		printKeypad(in, out, i + 1, j + 1);
 	}
 }
 
@@ -32,5 +36,9 @@ int main()
 	char in[100], out[100];
 	cin >> in;
 	printKeypad(in, out, 0, 0);
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+	}
 
 }
