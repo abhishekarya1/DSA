f673f20c043e41eae47730a862cbdeba9cb238ab
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-07-02 14:25:45 +0530

    Add

--- source/ProjReboot/oddeven.cc
+++ source/ProjReboot/oddeven.cc
@@ -46,24 +46,53 @@ node* createLL(int n)
 	return head;
 }
 
-node* oddeven(node* head)
+node* oddeven(node* &head)
 {
-	node* tl;
-	node* end = head;
-	node* cur = head;
-	node * prev = NULL;
-	while(cur->next)
-	{
-		if(cur->data % 2 == 0) { tl = cur; prev->next = cur->next;}
-		while(end->next)
-		{
-			end = end->next;
-		}
-		end->next = tl;
-		tl->next = NULL;
-		prev = cur;
-		cur = cur->next;
-	}
+    // Corner case
+    if (head == NULL)
+        return NULL;
+ 
+    // Initialize first nodes of even and
+    // odd lists
+    node *odd = head;
+    node *even = head->next;
+ 
+    // Remember the first node of even list so
+    // that we can connect the even list at the
+    // end of odd list.
+    node *evenFirst = even;
+ 
+    while (1)
+    {
+        // If there are no more nodes, then connect
+        // first node of even list to the last node
+        // of odd list
+        if (!odd || !even || !(even->next))
+        {
+            odd->next = evenFirst;
+            break;
+        }
+ 
+        // Connecting odd nodes
+        odd->next = even->next;
+        odd = even->next;
+ 
+        // If there are NO more even nodes after
+        // current odd.
+        if (odd->next == NULL)
+        {
+            even->next = NULL;
+            odd->next = evenFirst;
+            break;
+        }
+ 
+        // Connecting even nodes
+        even->next = odd->next;
+        even = odd->next;
+    }
+ 
+    return head;
+
 }
 
 int main()
@@ -72,5 +101,7 @@ int main()
 	node* head = createLL(n);
 	print(head);
 	oddeven(head);
+	print(head);
+
 
 }
