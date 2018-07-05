53dfaaf491757fb52e04e51f7342262e42b3d144
abhishekarya1 <abhishekarya1@users.noreply.github.com>
2018-07-05 23:56:35 +0530

    Add

---
+++ source/CDScrap/day1/div.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int x,y;
+	cout<<"Enter dividend : ";
+	cin>>x;
+	cout<<"Enter divisor : ";
+	cin>>y;
+	cout<<"Quotient is : "<<endl<<(double)x/y;    //cascading  //explicit type casting
+ 
+}		
